7a5991f4afcfc48011af1e030b82ecddf7a79b3f
RuanRCA <assuncaoruan7@gmail.com>
2025-02-05 11:28:33 -0300

    PrimeiroAp

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from "react";
+import {View ,Text , Image} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ /* class App extends Component{
+    render(){
+      let nome = 'Mercedez-Benz';
+      let imagem = 'https://www.hdcarwallpapers.com/download/mercedes_benz_cls_4k-1920x1080.jpg';
+      let profissao = 'Corredor';
+      return(
+        <View>
+          <Text style= {{ marginTop: 50,fontSize: 25,color:'#0000ff', textAlign: 'center' }}> {nome}  </Text>
+          <Image
+          source={{uri:imagem}}
+          style={{ width: 400 , height: 200}}></Image>
+            <Text style= {{ marginTop: 50,fontSize: 25,color:'#0000ff', textAlign: 'center' }}> {profissao}  </Text>
+
+           
+        </View>
+      );
+    }
+  }
+
+  
+
+
+export default App;*/
+
+class App extends Component{
+  render(){
+    let nome = 'Mercedez-Benz';
+    return(
+      <View>
+
+<Text style= {{ marginTop: 50,fontSize: 25,color:'#0000ff', textAlign: 'center' }}> {nome}  
+</Text>
+<Foto  largura={400} altura= {200} profissao='Corredor'></Foto>
+       
+      </View>
+ 
+    );
+  }
 }
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Foto extends Component{
+  render(){
+    let imagem = 'https://www.hdcarwallpapers.com/download/mercedes_benz_cls_4k-1920x1080.jpg';
+    return(
+      <View>
+          <Image source={{uri:imagem}}
+      style={{width:this.props.largura , height:this.props.altura}}></Image>
+      <Text>{this.props.profissao}</Text>
+
+
+      </View>
+    
+    );
+  }
+}
